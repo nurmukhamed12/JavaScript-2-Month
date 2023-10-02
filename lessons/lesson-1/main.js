15beed445c66d58df68315bd6bf94da265bddffc
mukha12 <mukha12>
2023-10-02 16:09:43 +0300

    first commit

---
+++ lessons/lesson-1/main.js
@@ -0,0 +1,57 @@
+
+
+
+let str = 'hello world!';
+let num = 10;
+let bool = true;
+let undef = undefined;
+let n = null;
+let obj = {};
+let num2 = '20'
+let i = 0
+i++;
+
+// BigInt
+// Symbol
+
+// console.log(typeof num2);
+// console.log(Number(num2));
+// console.log(5 % 2);
+// console.log(str + 10);
+// console.log('ba' + + 'abc' + 'a');
+// console.log(i);
+
+// num = 5
+// if(num > 5){
+//     console.log('hello world!');
+// } else if(num < 5){
+//     console.log('hello Geeks!');
+// }else {
+//     console.log('bye!');
+// }
+num = 4;
+if (num == 5){
+    console.log('В точку!');
+}else if(num == 4){
+    console.log('Мало!');
+}else if (num == 6){
+    console.log('Много!');
+}else {
+    console.log('Таких значений нет!');
+}
+
+
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// a = 15;
+// b = 25;
+
+// console.log(c);
+
+
+// let name = 'nurmuhamed'
+// let second_name
+// let age = 16
