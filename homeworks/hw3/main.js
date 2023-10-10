9de197a35253b6e2dc7d170cb0d461be3da19754
mukha12 <mukha12>
2023-10-10 16:42:57 +0600

    hw3

--- homeworks/hw3/main.js
+++ homeworks/hw3/main.js
@@ -1,4 +1,4 @@
-
+// основной
 
 function findLongestWord(str) {
     let words = str.match(/[а-яА-Яa-zA-ZёЁ]{1,}/gi);
@@ -14,18 +14,13 @@ function findLongestWord(str) {
   }
   console.log(findLongestWord('супра самая быстрая машина девяностых'));  
 
+// доп
 
-// Напишите функцию detect_data_type(value), которая принимает аргумент и возвращает тип.
-
-// Примечание. Существует шесть возможных значений, которые возвращает typeof: объект, логическое значение, функция, число, строка и неопределенное значение.
 function detect_data_type(value) {
     var type = typeof value;
     
-    if (type === 'object') {
-        if (Array.isArray(value)) {
-            return 'array';
-        }
-        if (value === null) {
+    if (type == 'object') {
+        if (value == null) {
             return 'null';
         }
         return 'object';
@@ -35,33 +30,9 @@ function detect_data_type(value) {
 }
 
 
-console.log(detect_data_type({
-    FirstName: 'nurmukhamed',
-    LastName: 'Abdraimov',
-    Secondname: null,
-    Patronomic: 'Talantbekovich',
-    FullName: 'Abdraimov Nurmukhamed Talantbekovich',
-    MidName: undefined,
-    gender: 'male',
-    age:42,}));
-console.log(detect_data_type(['baseball','hobbyhorsing','baskeball','chess']));
+console.log(detect_data_type({}));
 console.log(detect_data_type(null));
-console.log(detect_data_type(false)); 
-console.log(detect_data_type(function detect_data_type(value) {
-    var type = typeof value;
-    
-    if (type === 'object') {
-        if (Array.isArray(value)) {
-            return 'array';
-        }
-        if (value === null) {
-            return 'null';
-        }
-        return 'object';
-    }
-    
-    return type;
-}));
+console.log(detect_data_type(true));
+console.log(detect_data_type(function(){}));
 console.log(detect_data_type(42));
 console.log(detect_data_type("Hello, World!"));
-
